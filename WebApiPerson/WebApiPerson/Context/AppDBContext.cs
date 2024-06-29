55c8f5aba4235ee513294f530602f594876dfb27
alergio <alejosmaya8@gmail.com>
2024-06-29 20:46:35 -0300

    commit for simple api crud in .net

---
+++ WebApiPerson/WebApiPerson/Context/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
